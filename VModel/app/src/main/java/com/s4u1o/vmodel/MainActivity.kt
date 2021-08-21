9e0471f04723763e7623c70aa92896df5294aec7
s4u10 <saulobfilho@gmail.com>
2021-08-20 22:57:35 -0300

    Upload
    Upload

---
+++ VModel/app/src/main/java/com/s4u1o/vmodel/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.s4u1o.vmodel
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var txtContador: EditText
+    lateinit var btnDados: Button
+    lateinit var btnMostrar: Button
+
+    lateinit var mViewModel: MainViewModel
+
+    //  var contador: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //   logar(valor = "onCreate")
+
+        initDados()
+        //   initContador()
+        initClick()
+
+        //   validaContador()
+
+    }
+
+    private fun initDados() {
+        mViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        txtContador = findViewById(R.id.txtContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
+
+        mViewModel.mContador.observe(this, Observer { valor ->
+            txtContador.setText(valor)
+        })
+    }
+
+/*
+    override fun onStart() {
+        logar(valor = "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        logar(valor = "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        logar(valor = "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logar(valor = "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logar(valor = "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun logar(tag: String = "Ciclo de Vida", valor: String) {
+        Log.d(tag, valor)
+    }
+
+
+
+    private fun validaContador() {
+        if (contador > 5) {
+            contador = 0
+        }
+    }
+    */
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            mViewModel.Contador()
+            //    contador++
+            //   validaContador()
+            //   initContador()
+        }
+        btnMostrar.setOnClickListener {
+        //   Toast.makeText(this, "Valor Contador: ${contador.toString()}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Valor Contador: ${mViewModel.mContador.value}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+ /*  private fun initContador() {
+        txtContador.setText(contador.toString())
+    }
+ */
+}
---
+++ VModel/app/src/main/java/com/s4u1o/vmodel/MainViewModel.kt
@@ -0,0 +1,27 @@
+package com.s4u1o.vmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    var mContador = MutableLiveData<String>().apply { value = contador.toString() }
+
+    private var contador: Int = 0
+
+    private fun setmContador() {
+        mContador.value = contador.toString()
+    }
+
+    private fun validaContador() {
+        contador++
+        if (contador > 5) {
+            contador = 0
+        }
+
+        setmContador()
+    }
+
+    fun Contador(){
+        validaContador()
+    }
+}
